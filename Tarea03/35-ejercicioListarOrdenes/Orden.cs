6e97a6664b83cb1f7ec352104a6b18a9d967d16e
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2022-07-24 16:55:50 -0600

    Add files via upload

---
+++ Tarea03/35-ejercicioListarOrdenes/Orden.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+public class Orden
+{
+    public int Codigo { get; set; }
+    public DateTime Fecha { get; set; }
+    public string NumeroDeOrden { get; set; }
+    public Cliente Cliente { get; set; }
+    public Vendedor Vendedor { get; set; }
+    public List<OrdenDetalle> ListaOrdenDetalle { get; set; }
+    public double Total { get; set; }
+
+    public Orden(int codigo, DateTime fecha, string numeroDeOrden, Cliente cliente, Vendedor vendedor)
+    {
+        Codigo = codigo;
+        Fecha = fecha;
+        NumeroDeOrden = numeroDeOrden;
+        Cliente = cliente;
+        Vendedor = vendedor;
+        ListaOrdenDetalle = new List<OrdenDetalle>();
+    }
+
+    public void AgregarProducto(Producto producto)
+    {
+        int nuevoCodigo = ListaOrdenDetalle.Count + 1;
+        int cantidad = 1;
+        OrdenDetalle o = new OrdenDetalle(nuevoCodigo, 1, 1, producto);
+        ListaOrdenDetalle.Add(o);
+
+        Total += cantidad * producto.Precio;
+    }
+}
