ef02d99c92b22b1980a0b57314dcb93f6a2d4057
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2022-07-24 16:41:14 -0600

    Add files via upload

---
+++ Tarea03/25-ejercicioOrdenesCreandoClaseOrden/Orden.cs
@@ -0,0 +1,17 @@
+public class Orden
+{
+    public int Codigo { get; set; }
+    public DateTime Fecha { get; set; }
+    public string NumeroDeOrden { get; set; }
+    public Cliente Cliente { get; set; }
+    public Vendedor Vendedor { get; set; }
+
+    public Orden(int codigo, DateTime fecha, string numeroDeOrden, Cliente cliente, Vendedor vendedor)
+    {
+        Codigo = codigo;
+        Fecha = fecha;
+        NumeroDeOrden = numeroDeOrden;
+        Cliente = cliente;
+        Vendedor = vendedor;
+    }
+}
